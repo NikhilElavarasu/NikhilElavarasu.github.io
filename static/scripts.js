dbe776ef5b9b7fb5e91e482e3bd5b29717171031
NikhilElavarasu <rockingnikhil8@gmail.com>
2022-03-16 19:44:05 +0530

    carousel changes

--- static/scripts.js
+++ static/scripts.js
@@ -29,36 +29,65 @@ function formValidation(event) {
     }
 }
 
-const movButton = document.getElementsByClassName('movButton')
-const testimonials = document.getElementsByClassName('testimonial')
+const movButtonRight = document.getElementById('mov-button-right');
+const movButtonLeft = document.getElementById('mov-button-left');
+const testimonials = document.getElementsByClassName('testimonial');
 let testimonial_page = Math.ceil(testimonials.length / 2);
-let l = 0;
+let l = 101;
 let movePer = 101;
 let maxMove = 199;
 
 let mob_view = window.matchMedia("(max-width: 768px)");
 if (mob_view.matches) {
     movePer = 101;
-    maxMove = 407;
+    maxMove = 400;
 }
 
 let right_mover = () => {
-    l = l + movePer;
-    if (testimonials == 1) { l = 0; }
+    if (movButtonLeft.getAttribute('disabled') == 'true')
+        movButtonLeft.removeAttribute('disabled');
+
     for (const testimonial of testimonials) {
-        if (l > maxMove) { l = l - movePer; }
+        if (l > maxMove) {
+            break;
+        }
         testimonial.style.left = '-' + l + '%';
     }
 
+    l = l + movePer;
+    if (l > maxMove) {
+        movButtonRight.setAttribute("disabled", "true");
+        l = l - movePer;
+        return
+    }
 }
+
 let left_mover = () => {
     l = l - movePer;
-    if (l <= 0) { l = 0; }
+    if (l <= 0) {
+        l = 0;
+    }
+
+    if (movButtonRight.getAttribute('disabled') == 'true')
+        movButtonRight.removeAttribute('disabled')
+
     for (const testimonial of testimonials) {
         if (testimonial_page > 1) {
             testimonial.style.left = '-' + l + '%';
         }
     }
+
+    if (l < movePer) {
+        movButtonLeft.setAttribute('disabled', 'true')
+        l = l + movePer;
+    }
 }
-movButton[1].onclick = () => { right_mover(); }
-movButton[0].onclick = () => { left_mover(); }
+
+movButtonRight.onclick = () => {
+    if (l < maxMove) {
+        right_mover();
+    }
+}
+movButtonLeft.onclick = () => {
+    left_mover();
+}
